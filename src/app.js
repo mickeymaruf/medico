be53c895184a221935e4e469f5577cfc91285ab4
mickeymaruf <mickeymaruf@gmail.com>
2026-02-08 23:09:32 +0600

    feat: add Prisma integration

---
+++ src/app.js
@@ -0,0 +1,22 @@
+// import cookieParser from "cookie-parser";
+import express from "express";
+// import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
+// import { notFound } from "./app/middleware/notFound";
+// import { IndexRoutes } from "./app/routes";
+const app = express();
+// Enable URL-encoded form data parsing
+app.use(express.urlencoded({ extended: true }));
+// Middleware to parse JSON bodies
+app.use(express.json());
+// app.use(cookieParser())
+// app.use("/api/v1", IndexRoutes);
+// Basic route
+app.get("/", async (req, res) => {
+    res.status(201).json({
+        success: true,
+        message: "API is working",
+    });
+});
+// app.use(globalErrorHandler)
+// app.use(notFound)
+export default app;
