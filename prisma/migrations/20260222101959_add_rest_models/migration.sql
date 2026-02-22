e68fbddaa98c50e67a60e394cd93a9c1ce1c09e6
mickeymaruf <mickeymaruf@gmail.com>
2026-02-22 16:20:56 +0600

    feat: add more models & relations

---
+++ prisma/migrations/20260222101959_add_rest_models/migration.sql
@@ -0,0 +1,211 @@
+-- CreateEnum
+CREATE TYPE "AppointmentStatus" AS ENUM ('SCHEDULED', 'INPROGRESS', 'COMPLETED', 'CANCELED');
+
+-- CreateEnum
+CREATE TYPE "PaymentStatus" AS ENUM ('PAID', 'UNPAID');
+
+-- CreateEnum
+CREATE TYPE "BloodGroup" AS ENUM ('A_POSITIVE', 'A_NEGATIVE', 'B_POSITIVE', 'B_NEGATIVE', 'AB_POSITIVE', 'AB_NEGATIVE', 'O_POSITIVE', 'O_NEGATIVE');
+
+-- CreateEnum
+CREATE TYPE "MaritalStatus" AS ENUM ('MARRIED', 'UNMARRIED', 'DIVORCED', 'WIDOWED');
+
+-- CreateTable
+CREATE TABLE "admins" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "profilePhoto" TEXT,
+    "contactNumber" TEXT,
+    "address" TEXT,
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "admins_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "appointments" (
+    "id" TEXT NOT NULL,
+    "patientId" TEXT NOT NULL,
+    "doctorScheduleId" TEXT NOT NULL,
+    "doctorId" TEXT NOT NULL,
+    "videoCallingId" TEXT NOT NULL,
+    "status" "AppointmentStatus" NOT NULL DEFAULT 'SCHEDULED',
+    "paymentStatus" "PaymentStatus" NOT NULL DEFAULT 'UNPAID',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "appointments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "medical_reports" (
+    "id" TEXT NOT NULL,
+    "patientId" TEXT NOT NULL,
+    "doctorId" TEXT,
+    "reportName" TEXT NOT NULL,
+    "reportLink" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "medical_reports_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "patient_health_data" (
+    "id" TEXT NOT NULL,
+    "patientId" TEXT NOT NULL,
+    "gender" "Gender" NOT NULL,
+    "dateOfBirth" TIMESTAMP(3) NOT NULL,
+    "bloodGroup" "BloodGroup" NOT NULL,
+    "hasAllergies" BOOLEAN NOT NULL DEFAULT false,
+    "hasDiabetes" BOOLEAN NOT NULL DEFAULT false,
+    "height" TEXT NOT NULL,
+    "weight" TEXT NOT NULL,
+    "smokingStatus" BOOLEAN NOT NULL DEFAULT false,
+    "dietaryPreferences" TEXT,
+    "pregnancyStatus" BOOLEAN NOT NULL DEFAULT false,
+    "mentalHealthHistory" TEXT,
+    "immunizationStatus" TEXT,
+    "hasPastSurgeries" BOOLEAN NOT NULL DEFAULT false,
+    "recentAnxiety" BOOLEAN NOT NULL DEFAULT false,
+    "recentDepression" BOOLEAN NOT NULL DEFAULT false,
+    "maritalStatus" "MaritalStatus",
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "patient_health_data_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "reviews" (
+    "id" TEXT NOT NULL,
+    "appointmentId" TEXT NOT NULL,
+    "patientId" TEXT NOT NULL,
+    "doctorId" TEXT NOT NULL,
+    "rating" DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    "comment" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "reviews_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "schedules" (
+    "id" TEXT NOT NULL,
+    "startDateTime" TIMESTAMP(3) NOT NULL,
+    "endDateTime" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "schedules_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "doctor_schedules" (
+    "doctorId" TEXT NOT NULL,
+    "scheduleId" TEXT NOT NULL,
+    "isBooked" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "doctor_schedules_pkey" PRIMARY KEY ("doctorId","scheduleId")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "admins_email_key" ON "admins"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "admins_userId_key" ON "admins"("userId");
+
+-- CreateIndex
+CREATE INDEX "admins_email_idx" ON "admins"("email");
+
+-- CreateIndex
+CREATE INDEX "admins_isDeleted_idx" ON "admins"("isDeleted");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "appointments_videoCallingId_key" ON "appointments"("videoCallingId");
+
+-- CreateIndex
+CREATE INDEX "appointments_patientId_idx" ON "appointments"("patientId");
+
+-- CreateIndex
+CREATE INDEX "appointments_doctorId_idx" ON "appointments"("doctorId");
+
+-- CreateIndex
+CREATE INDEX "appointments_doctorScheduleId_idx" ON "appointments"("doctorScheduleId");
+
+-- CreateIndex
+CREATE INDEX "appointments_status_idx" ON "appointments"("status");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "appointments_doctorId_doctorScheduleId_key" ON "appointments"("doctorId", "doctorScheduleId");
+
+-- CreateIndex
+CREATE INDEX "medical_reports_patientId_idx" ON "medical_reports"("patientId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "patient_health_data_patientId_key" ON "patient_health_data"("patientId");
+
+-- CreateIndex
+CREATE INDEX "patient_health_data_patientId_idx" ON "patient_health_data"("patientId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "reviews_appointmentId_key" ON "reviews"("appointmentId");
+
+-- CreateIndex
+CREATE INDEX "reviews_appointmentId_idx" ON "reviews"("appointmentId");
+
+-- CreateIndex
+CREATE INDEX "reviews_patientId_idx" ON "reviews"("patientId");
+
+-- CreateIndex
+CREATE INDEX "reviews_doctorId_idx" ON "reviews"("doctorId");
+
+-- CreateIndex
+CREATE INDEX "doctor_schedules_doctorId_idx" ON "doctor_schedules"("doctorId");
+
+-- CreateIndex
+CREATE INDEX "doctor_schedules_scheduleId_idx" ON "doctor_schedules"("scheduleId");
+
+-- AddForeignKey
+ALTER TABLE "admins" ADD CONSTRAINT "admins_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "appointments" ADD CONSTRAINT "appointments_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "patient"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "appointments" ADD CONSTRAINT "appointments_doctorId_fkey" FOREIGN KEY ("doctorId") REFERENCES "doctor"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "appointments" ADD CONSTRAINT "appointments_doctorId_doctorScheduleId_fkey" FOREIGN KEY ("doctorId", "doctorScheduleId") REFERENCES "doctor_schedules"("doctorId", "scheduleId") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "medical_reports" ADD CONSTRAINT "medical_reports_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "patient"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "medical_reports" ADD CONSTRAINT "medical_reports_doctorId_fkey" FOREIGN KEY ("doctorId") REFERENCES "doctor"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "patient_health_data" ADD CONSTRAINT "patient_health_data_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "patient"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reviews" ADD CONSTRAINT "reviews_appointmentId_fkey" FOREIGN KEY ("appointmentId") REFERENCES "appointments"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reviews" ADD CONSTRAINT "reviews_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "patient"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reviews" ADD CONSTRAINT "reviews_doctorId_fkey" FOREIGN KEY ("doctorId") REFERENCES "doctor"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "doctor_schedules" ADD CONSTRAINT "doctor_schedules_doctorId_fkey" FOREIGN KEY ("doctorId") REFERENCES "doctor"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "doctor_schedules" ADD CONSTRAINT "doctor_schedules_scheduleId_fkey" FOREIGN KEY ("scheduleId") REFERENCES "schedules"("id") ON DELETE CASCADE ON UPDATE CASCADE;
